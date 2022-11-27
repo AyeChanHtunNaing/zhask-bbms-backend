569af0d817a28094088a9a04446c175d0b8ce361
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2022-11-27 13:23:04 +0630

    update

--- src/main/java/com/bbms/config/SecurityContants.java
+++ src/main/java/com/bbms/config/SecurityContants.java
@@ -3,6 +3,6 @@ package com.bbms.config;
 public class SecurityContants {
 	public static final String JWT_KEY = "bm9uZSBvZiB5b3VyIGYqa2luZyBidXNpbmVzcw==";
 	public static final String JWT_HEADER = "Authorization";
-	public static final String FRONTEND_BASE_URL = "http://localhost:4200";
+	public static final String FRONTEND_BASE_URL = "https://zhask-bbms.netlify.app/";
 	public static final String BACKEND_BASE_URL = "http://localhost:8080";
 }
